af202c789cc4641ad950217a565843ffbca6226a
Brad Oyler <bradoyler@gmail.com>
2017-08-14 13:00:34 -0400

    update tests for NVCA score

--- test/index.js
+++ test/index.js
@@ -5,15 +5,17 @@ const defendants = require('./defendants.json')
 defendants.forEach(function (defendant, idx) {
   const FTA = psaCheck.ftaRiskScore(defendant, 'scaled')
   const NCA = psaCheck.ncaRiskScore(defendant, 'scaled')
+  const NVCA = psaCheck.isNvcaRisk(defendant, 'scaled')
   const verdict = psaCheck(defendant)
   console.log(`
     Case #${defendant.case}
     - Crime: ${defendant.crime}
-    - Scores: [ FTA:${FTA}, NCA:${NCA} ]
+    - Scores: [ FTA:${FTA}, NCA:${NCA}, NVCA:${NVCA} ]
     - Recommendation ${verdict.code}, ${verdict.text}
     -----------------------------------------
     `)
   assert.equal(FTA, defendant.expected.FTA)
   assert.equal(NCA, defendant.expected.NCA)
+  assert.equal(NVCA, defendant.expected.NVCA)
   assert.equal(verdict.code, defendant.expected.verdict)
 })
